d71b850d87331228be516c75eb15bbce15d34d78
Keelan Burnham <kburnham14@icloud.com>
2025-05-16 19:41:08 -0500

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Keelan Burnham on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherForecastTests/WeatherForecastTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastTests.swift
+//  WeatherForecastTests
+//
+//  Created by Keelan Burnham on 5/16/25.
+//
+
+import Testing
+@testable import WeatherForecast
+
+struct WeatherForecastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
